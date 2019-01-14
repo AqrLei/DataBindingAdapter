f295badeb696bf2a4e0acf125ac08a4b74df8c68
AqrLei <leilei@guoshujinfu.com>
2019-01-14 18:26:47 +0800

    MutiType

--- bindingadapter/src/main/kotlin/com/aqrlei/open/bindingadapter/collections/MultiTypeObservableList.kt
+++ bindingadapter/src/main/kotlin/com/aqrlei/open/bindingadapter/collections/MultiTypeObservableList.kt
@@ -1,30 +1,53 @@
 package com.aqrlei.open.bindingadapter.collections
 
+import androidx.databinding.ListChangeRegistry
 import androidx.databinding.ObservableList
 import java.util.AbstractList
+import kotlin.collections.ArrayList
 
 /**
  * @author aqrlei on 2019/1/14
  * Collections.singletonList 只有一个item时的内存优化"
  */
-class MultiTypeObservableList<T>:AbstractList<T>(),ObservableList<T>{
-    override fun removeOnListChangedCallback(callback: ObservableList.OnListChangedCallback<out ObservableList<T>>?) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+class MultiTypeObservableList<T> : AbstractList<T>(), ObservableList<T> {
+    private val listeners = ListChangeRegistry()
+    private val callback = ListChangeCallBack<T>()
+    private val lists: ArrayList<List<out T>> = ArrayList()
+    override fun removeOnListChangedCallback(callback: ObservableList.OnListChangedCallback<out ObservableList<T>>) {
+        listeners.remove(callback)
     }
 
     override fun addOnListChangedCallback(callback: ObservableList.OnListChangedCallback<out ObservableList<T>>?) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        listeners.add(callback)
     }
 
     override val size: Int
-        get() = TODO("not implemented") //To change initializer of created properties use File | Settings | File Templates.
+        get() {
+            var tempSize = 0
+            for (list in lists) {
+                tempSize += list.size
+            }
+            return tempSize
+        }
 
     override fun get(index: Int): T {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        if (index < 0) {
+            throw IndexOutOfBoundsException()
+        }
+        var tempSize = 0
+        for (list in lists) {
+            if (index - tempSize < list.size) {
+                return list[index - tempSize]
+            }
+            tempSize += list.size
+        }
+        throw java.lang.IndexOutOfBoundsException()
     }
-    internal class ListChangeCallBack<T>:ObservableList.OnListChangedCallback<ObservableList<T>>(){
+
+    inner class ListChangeCallBack<T> : ObservableList.OnListChangedCallback<ObservableList<T>>() {
         override fun onChanged(sender: ObservableList<T>?) {
-            TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+            modCount++
+            listeners.notifyChanged(this@MultiTypeObservableList)
         }
 
         override fun onItemRangeRemoved(sender: ObservableList<T>?, positionStart: Int, itemCount: Int) {
